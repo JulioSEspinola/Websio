6b4814bbe9cde5249308f70dbac03f51c7f878a9
Julio S Espinola <52016566+JulioSEspinola@users.noreply.github.com>
2025-02-06 12:11:44 -0800

    added new responsiveness to the menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/Header.module.css';
 import logo from '../assets/websio.png'; // Adjust the path as needed
 
 function Header({ scrollToSection }) {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.logoContainer}>
         <img src={logo} alt="Websio Logo" className={styles.logo} />
       </div>
       <nav className={styles.nav}>
-        <ul className={styles.navList}>
-          <li><button onClick={() => scrollToSection('home')} className={styles.navLink}>Home</button></li>
-          <li><button onClick={() => scrollToSection('services')} className={styles.navLink}>Services</button></li>
-          <li><button onClick={() => scrollToSection('about')} className={styles.navLink}>About</button></li>
-          <li><button onClick={() => scrollToSection('contact')} className={styles.navLink}>Contact</button></li>
+        <div className={styles.menuToggle} onClick={toggleMenu}>
+          <button className={styles.menuToggle}>Menu</button>
+        </div>
+        <ul className={`${styles.navList} ${menuOpen ? styles.active : ''}`}>
+          <li><button onClick={() => { scrollToSection('home'); toggleMenu(); }} className={styles.navLink}>Home</button></li>
+          <li><button onClick={() => { scrollToSection('services'); toggleMenu(); }} className={styles.navLink}>Services</button></li>
+          <li><button onClick={() => { scrollToSection('about'); toggleMenu(); }} className={styles.navLink}>About</button></li>
+          <li><button onClick={() => { scrollToSection('contact'); toggleMenu(); }} className={styles.navLink}>Contact</button></li>
         </ul>
       </nav>
     </header>
